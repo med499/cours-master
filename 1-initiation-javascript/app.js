5742856f57f1995b75f1a1c7ea20b4252beac7f2
Jacob <jacob.dev.mail@gmail.com>
2021-03-08 20:57:49 +0100

    solution exercice des villes

---
+++ 1-initiation-javascript/app.js
@@ -0,0 +1,52 @@
+/**
+ * déclarer la classe City
+ */
+class City {
+  // le constructeur de la classe
+  constructor (name, country, lat, lon, population) {
+    // les paramétres de la classe
+    this.name = name;
+    this.country = country;
+    this.lat = lat;
+    this.lon = lon;
+    this.population = population;
+  }
+
+  // les méthodes de la classe
+  getDetails () {
+    return this.name + ', ' + this.country + '. Population: ' + this.population + ' habitants.';
+  }
+  getPosition () {
+    return {
+      lat: this.lat,
+      lon: this.lon
+    }
+  }
+}
+
+/**
+ * créer (instancier) les objets
+ */
+
+// la ville de paris
+var paris = new City('Paris', 'France',	48.85,	2.27,	2175601);
+
+console.log(paris.getDetails()); // -> Paris, France. Population: 2175601 habitants.
+console.log(paris.getPosition()); // -> {lat: 48.85, lon: 2.27}
+
+// les autres villes
+var madrid = new City('Madrid', 'Espagne',	40.44,	-3.82,	3334730);
+var rome = new City('Rome', 'Italie',	41.91,	12.40,	2844395);
+var berlin = new City('Berlin', 'Allemagne',	52.51,	13.15,	3748148);
+
+/**
+ * créer une liste de villes
+ */
+var cities = [];
+cities.push(paris);
+cities.push(madrid);
+cities.push(rome);
+cities.push(berlin);
+
+console.log(cities); // -> (4) [City, City, City, City]
+
